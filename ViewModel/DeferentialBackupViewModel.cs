4e9c0345e5ddd5da1a698344b5281839d2de3aa8
Baha Eddine Alili <BahaEddine.Alili.dz@viacesi.fr>
2022-12-16 16:25:51 +0100

    Add project files.

---
+++ ViewModel/DeferentialBackupViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySavev3G5.ViewModel
+{
+    internal class DeferentialBackupViewModel
+    {
+
+    }
+}
